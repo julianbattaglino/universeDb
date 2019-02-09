8feb5746510766bff51f445aac1e3f5fef9496b0
Julian <julianbattaglino@gmail.com>
2019-02-09 17:08:22 -0300

    Screen Splash / New Launcher Login UI

--- app/src/main/java/com/example/julian/universedb/Launcher.java
+++ app/src/main/java/com/example/julian/universedb/Launcher.java
@@ -6,15 +6,13 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.CardView;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
 import android.widget.TextView;
+
+import com.airbnb.lottie.LottieAnimationView;
 
 
 public class Launcher extends AppCompatActivity {
 
-    boolean isActividadSelected = false;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -22,18 +20,22 @@ public class Launcher extends AppCompatActivity {
         setContentView(R.layout.launcher);
 
 
+        LottieAnimationView button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(i);
+            }
+        });
+
         TextView poweredby = findViewById(R.id.powered);
         TextView dharma = findViewById(R.id.dharma);
         TextView universedb = findViewById(R.id.universedb);
 
 
         CardView cardviewtitle = findViewById(R.id.cardviewtitle);
-        CardView cardviewspinner = findViewById(R.id.cardviewspinner);
-        Spinner spinner = findViewById(R.id.spinner);
 
-
-
-        AnimationUtils.enterLeft(cardviewspinner, 1500);
 
         AnimationUtils.enterRight(universedb, 2000);
         AnimationUtils.enterLeft(cardviewtitle, 2100);
@@ -42,31 +44,6 @@ public class Launcher extends AppCompatActivity {
         AnimationUtils.enterLeft(poweredby, 2200);
         AnimationUtils.enterLeft(dharma, 2000);
         AnimationUtils.rotateX(dharma, 2200);
-
-
-        spinner.setAdapter(new ArrayAdapter<>(this, R.layout.support_simple_spinner_dropdown_item, getResources().getStringArray(R.array.spinner_actividades)));
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-
-            @Override
-            public void onItemSelected(AdapterView<?> adapterView, View view, int pos, long id) {
-                if (isActividadSelected) {
-                    openMainActivity(pos);
-                }
-                isActividadSelected = true;
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
-            }
-        });
-    }
-
-    private void openMainActivity(int pos) {
-        //Toast.makeText(adapterView.getContext(), (String) adapterView.getItemAtPosition(pos), Toast.LENGTH_SHORT).show();
-        Intent i = new Intent(Launcher.this, MainActivity.class);
-        i.putExtra("", pos);
-        startActivity(i);
 
 
     }
---
+++ app/src/main/java/com/example/julian/universedb/SplashScreenActivity.java
@@ -0,0 +1,27 @@
+package com.example.julian.universedb;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), Launcher.class));
+            }
+        }, 4000);
+    }
+}
+
+
